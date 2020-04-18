0412c498a6fcac0d2fe51f27946c2c2b01256e36
Edvaldo Segundo <edvaldo.segundo@hotmail.com>
2020-04-18 22:26:40 +0100

    Projeto criado

---
+++ src/test/java/com/ed2nd/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ed2nd.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
