460b3f1d1c6cc7478e804d4b626e7f5f945bd89d
Edvaldo Segundo <edvaldo.segundo@hotmail.com>
2020-05-10 15:38:33 +0100

    Criando profile de desenvolvimento

--- src/main/java/com/ed2nd/cursomc/config/DevConfig.java
+++ src/main/java/com/ed2nd/cursomc/config/DevConfig.java
@@ -3,7 +3,6 @@ package com.ed2nd.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,26 +10,20 @@ import org.springframework.context.annotation.Profile;
 import com.ed2nd.cursomc.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
 	@Autowired
 	private DBService dbService;
-	
-	@Value("${spring.jpa.hibernate.ddl-auto}")
-	private String strategy;
 
 	@Bean
 	public boolean instantiateDataBase() throws ParseException {
-		
-		if(!"create".equals(strategy)) {
-			return false;
-		}
-		
 		dbService.instantiateTestDatabase();
 		return true;
 
 	}
 }
+
+
 
 
